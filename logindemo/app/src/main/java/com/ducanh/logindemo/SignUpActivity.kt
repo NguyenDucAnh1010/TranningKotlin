64f9231734c275e5bcf1c15621f9909574b55637
NguyenDucAnh1010 <ducnguyenanh2003@gmail.com>
2025-02-25 10:37:14 +0700

    update

--- logindemo/app/src/main/java/com/ducanh/logindemo/SignUpActivity.kt
+++ logindemo/app/src/main/java/com/ducanh/logindemo/SignUpActivity.kt
@@ -1,29 +1,20 @@
 package com.ducanh.logindemo
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val sign_Up_Txt = findViewById<TextView>(R.id.sign_Up_Txt)
-
-        sign_Up_Txt.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
         }
     }
 }
