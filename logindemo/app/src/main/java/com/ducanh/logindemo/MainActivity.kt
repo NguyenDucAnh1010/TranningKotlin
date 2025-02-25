4016de37931faa4b8ebf8b9996b3a5c7a53b7ba4
NguyenDucAnh1010 <ducnguyenanh2003@gmail.com>
2025-02-25 14:46:09 +0700

    update

--- logindemo/app/src/main/java/com/ducanh/logindemo/MainActivity.kt
+++ logindemo/app/src/main/java/com/ducanh/logindemo/MainActivity.kt
@@ -2,13 +2,19 @@ package com.ducanh.logindemo
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sign_UpTxt: TextView
+    private lateinit var emailIdEdt: TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,11 +25,19 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val sign_Up_Txt = findViewById<TextView>(R.id.sign_Up_Txt)
+        sign_UpTxt = findViewById(R.id.signUpTxt)
+        emailIdEdt = findViewById(R.id.emailIdEdt)
 
-        sign_Up_Txt.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+        sign_UpTxt.setOnClickListener {
+            Intent(this, SignUpActivity::class.java).apply {
+                startActivity(this)
+                finish()
+            }
+
+        }
+
+        intent.getStringExtra("email")?.let {
+            emailIdEdt.setText(it)
         }
     }
 }
--- logindemo/app/src/main/java/com/ducanh/logindemo/SignUpActivity.kt
+++ logindemo/app/src/main/java/com/ducanh/logindemo/SignUpActivity.kt
@@ -1,12 +1,29 @@
 package com.ducanh.logindemo
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.method.PasswordTransformationMethod
+import android.util.Patterns
+import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var signInTxt: TextView
+    private lateinit var backBtn: ImageButton
+    private lateinit var passwordEdt: TextInputEditText
+    private lateinit var firstNameEdt: TextInputEditText
+    private lateinit var lastNameEdt: TextInputEditText
+    private lateinit var emailIdEdt: TextInputEditText
+    private lateinit var createAcountBtn: AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +32,77 @@ class SignUpActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        signInTxt = findViewById(R.id.signInTxt)
+        backBtn = findViewById(R.id.backBtn)
+        passwordEdt = findViewById(R.id.passwordEdt)
+        firstNameEdt = findViewById(R.id.firstNameEdt)
+        lastNameEdt = findViewById(R.id.lastNameEdt)
+        emailIdEdt = findViewById(R.id.emailIdEdt)
+        createAcountBtn = findViewById(R.id.createAcountBtn)
+
+        signInTxt.setOnClickListener {
+            Intent(this, MainActivity::class.java).apply {
+                startActivity(this)
+                finish()
+            }
+        }
+
+        backBtn.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+
+        passwordEdt.transformationMethod = object : PasswordTransformationMethod() {
+            override fun getTransformation(source: CharSequence, view: View?): CharSequence {
+                return PasswordCharSequence(source)
+            }
+        }
+
+        createAcountBtn.setOnClickListener {
+            checkValid()
+        }
+    }
+
+    private fun checkValid() {
+        val firstName = firstNameEdt.text.toString().trim()
+        val lastName = lastNameEdt.text.toString().trim()
+        val emailId = emailIdEdt.text.toString().trim()
+
+        when {
+            firstName.isEmpty() -> {
+                Toast.makeText(this, "First name must be not empty", Toast.LENGTH_SHORT).show()
+            }
+
+            lastName.isEmpty() -> {
+                Toast.makeText(this, "First name must be not empty", Toast.LENGTH_SHORT).show()
+            }
+
+            !Patterns.EMAIL_ADDRESS.matcher(emailId).matches() -> {
+                Toast.makeText(this, "Email is not valid", Toast.LENGTH_SHORT).show()
+            }
+
+            else -> {
+                Toast.makeText(this, "Create Account Success!", Toast.LENGTH_SHORT).show()
+                Intent(this, MainActivity::class.java).apply {
+                    putExtra("email", emailId)
+                    startActivity(this)
+                }
+            }
+        }
+    }
+
+    class PasswordCharSequence(private val source: CharSequence) : CharSequence {
+        override val length: Int
+            get() = source.length
+
+        override fun get(index: Int): Char {
+            return '*'
+        }
+
+        override fun subSequence(startIndex: Int, endIndex: Int): CharSequence {
+            return source.subSequence(startIndex, endIndex)
         }
     }
 }
